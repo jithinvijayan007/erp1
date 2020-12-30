33b9587ad19d194f7d322cfe61e8b183649dda7e
Fahad <fahad@travidux.in>
2020-12-30 12:27:04 +0530

    sql

---
+++ ERP.0.1.sql
@@ -0,0 +1,14 @@
+alter table branch add column fk_hierarchy_data bigint REFERENCES hierarchy_data(pk_bint_id);
+create table hierarchy(
+  pk_bint_id  BIGSERIAL PRIMARY KEY,
+  int_level smallint,
+  vchr_name varchar(100)
+  );
+
+create table hierarchy_data(
+  pk_bint_id  BIGSERIAL PRIMARY KEY,
+  vchr_name varchar(100),
+  vchr_code varchar(5),
+  fk_hierarchy_id bigint REFERENCES hierarchy(pk_bint_id),
+  fk_hierarchy_data_id bigint REFERENCES hierarchy_data(pk_bint_id)
+  );
