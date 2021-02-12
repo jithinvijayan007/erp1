075ab78294e2a83683717fd50fbc98d905076411
Fahad <fahad@travidux.in>
2021-02-12 17:52:39 +0530

    product report

--- reports/urls.py
+++ reports/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import DailySalesReport,CustomerTypeahead,DailyBranchStockReport,ClientOutstandingReport,DetailedSalesReport,gdotreport,RechargeProfitReport,ProductProfitReport,SmartChoiceReport,PurchaseReport,BranchStockHistory,SmartChoiceSaleReport
 from .views2 import CreditSaleReport,StockTransferHistoryExport
+from .views_bi import ServiceReportMobile
 urlpatterns = [
     url(r'^daily_sales_report/$', DailySalesReport.as_view(), name='daily_sales_report'),
     url(r'^customer_typeahead/$', CustomerTypeahead.as_view(), name='customer_typeahead'),
@@ -16,6 +17,8 @@ urlpatterns = [
     url(r'^smart_choice_sale_report/$',SmartChoiceSaleReport.as_view(), name='SmartChoiceSalesReport'),
     url(r'^credit_sale_report/$',CreditSaleReport.as_view(), name='credit_sale_report'),
     url(r'^stock_transfer_history/$',StockTransferHistoryExport.as_view(), name='stock_transfer_history'),
+    # url(r'^service_wise_mobile/$','afsdf', name='service_wise_mobile'),
+    url(r'^service_wise_mobile/$',ServiceReportMobile.as_view(),name='service_wise_mobile'),
 
     #url(r'^purchase_report/$',PurchaseReport.as_view(), name='PurchaseReport'),
 
